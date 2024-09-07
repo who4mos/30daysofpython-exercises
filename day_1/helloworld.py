5ac4191c1530f3f20b2e5068804588e529ce3b44
who4mos <126173004+who4mos@users.noreply.github.com>
2024-09-07 01:13:36 -0300

    initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,21 @@
+print(3 + 4)  # 7
+print(3 - 4)  # -1
+print(3 * 4)  # 12
+print(3 % 4)  # 3
+print(3 / 4)  # .75
+print(3 ** 4) # 81
+print(3 // 4) # 0
+
+print("Gabriel")
+print("Ramos")
+print("Brasil")
+print("Yea")
+
+print(type(10))     # int
+print(type(9.8))    # float
+print(type(3.14))   # float
+print(type(4 - 4j)) # complex
+print(type(['Asabeneh', 'Python', 'Finland'])) # list
+print(type("Gabriel")) # string
+print(type("Ramos")) # string
+print(type("Brasil")) # string
---
+++ day_2/variables.py
@@ -0,0 +1,70 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Red"
+
+last_name = "Master"
+
+full_name = first_name + last_name
+
+country = "Pallet"
+
+city = "Lavender"
+
+age = 420
+
+year = 1969
+
+is_maried = True
+
+is_true = True
+
+is_light_on = False
+
+a, b =  420, 69
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_maried))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+
+print(len(first_name))
+
+print(len(first_name), len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+divison = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+r = 30
+
+area_of_circle = 3.1415 * r ** 2
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.1415 * r
+print(circum_of_circle)
+
+r = float(input("Circle radius: "))
+area_of_circle = 3.1415 * r ** 2
+print(area_of_circle)
+
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+country = input("Country: ")
+age = int(input("Age: "))
+
+print(help("keywords"))
